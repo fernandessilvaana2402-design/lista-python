0445c0a6694e1071c5040918737b99f831282364
fernandessilvaana2402-design <fernandes.silva.ana2402@escola.pr.gov.br>
2025-09-19 10:34:35 -0300

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -2,11 +2,11 @@ contagem_pares = 0
 contagem_impar = 0
 
 for i in range (10):
-numero = int(imput(f"por fvor insira o{i + 1}numro inteio: "))
-i numero % 2 == 0
-       contagem_pares += 1
-  else
-       contagem_impar += 1
+    numero = int(imput(f"por fvor insira o{i + 1}numro inteio: "))
+    if numero % 2 == 0:
+        contagem_pares += 1
+    else:
+        contagem_impar += 1
 
 
 print("total de numeros pares {contagem_pares}")
