f801f2d26b351a0d5e6b11fb114395eedf0dfab4
fernandessilvaana2402-design <fernandes.silva.ana2402@escola.pr.gov.br>
2025-09-19 10:28:16 -0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,13 @@
+contagem_pares = 0
+contagem_impar = 0
+
+for i in range (10):
+numero = int(imput(f"por fvor insira o{i + 1}numro inteio: "))
+i numero % 2 == 0
+       contagem_pares += 1
+  else
+       contagem_impar += 1
+
+
+print("total de numeros pares {contagem_pares}")
+print("total de numeros impares {vontagem_impar}")
